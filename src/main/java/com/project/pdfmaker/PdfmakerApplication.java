86f639da7d3fb6e6f97d75008f1f1bc0ca0102b3
madhavag2002 <madhav2002agarwal@gmail.com>
2024-11-05 18:46:12 +0530

    Swagger-ui

--- src/main/java/com/project/pdfmaker/PdfmakerApplication.java
+++ src/main/java/com/project/pdfmaker/PdfmakerApplication.java
@@ -4,7 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Madhav's do docs",
+        version = "3.0.0",
+        description = "API for manipulating PDF documents",
+        contact = @Contact(
+            name = "API Support",
+            email = "support@pdfmaker.com",
+            url = "https://pdfmaker.com/contact"
+        )
+    )
+)
 public class PdfmakerApplication {
 	@Value("${spring.data.redis.host}")
 	String test;
